0c97c4bb224885f6960f1cfa41c9c83d099315a6
matthew <marod424@gmail.com>
2022-07-06 12:34:41 -0400

    (refactor): improve dpk readability

--- dpk.js
+++ dpk.js
@@ -1,28 +1,25 @@
 const crypto = require("crypto");
+const TRIVIAL_PARTITION_KEY = "0";
+const MAX_PARTITION_KEY_LENGTH = 256;
 
 exports.deterministicPartitionKey = (event) => {
-  const TRIVIAL_PARTITION_KEY = "0";
-  const MAX_PARTITION_KEY_LENGTH = 256;
   let candidate;
 
   if (event) {
-    if (event.partitionKey) {
-      candidate = event.partitionKey;
-    } else {
-      const data = JSON.stringify(event);
-      candidate = crypto.createHash("sha3-512").update(data).digest("hex");
-    }
+    candidate = getCandidateFromEvent(event);
   }
 
-  if (candidate) {
-    if (typeof candidate !== "string") {
-      candidate = JSON.stringify(candidate);
-    }
-  } else {
-    candidate = TRIVIAL_PARTITION_KEY;
-  }
-  if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
-    candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
-  }
-  return candidate;
+  return handleCandidateLength(candidate);
 };
+
+function getCandidateFromEvent(event) {
+  return event.partitionKey ?? crypto.createHash("sha3-512").update(JSON.stringify(event)).digest("hex")
+}
+
+function handleCandidateLength(candidate = TRIVIAL_PARTITION_KEY) {
+  const candidateStr = typeof candidate !== "string" ? JSON.stringify(candidate) : candidate;
+
+  return candidateStr.length > MAX_PARTITION_KEY_LENGTH 
+    ? crypto.createHash("sha3-512").update(candidateStr).digest("hex") 
+    : candidateStr;
+}
--- dpk.test.js
+++ dpk.test.js
@@ -1,8 +1,34 @@
 const { deterministicPartitionKey } = require("./dpk");
+const crypto = require("crypto");
 
 describe("deterministicPartitionKey", () => {
   it("Returns the literal '0' when given no input", () => {
     const trivialKey = deterministicPartitionKey();
     expect(trivialKey).toBe("0");
+  });
+
+  it("Returns event 'partitionKey' when event has one", () => {
+    const partitionKey = "testKey"
+
+    const eventPartitionKey = deterministicPartitionKey({ partitionKey });
+    expect(eventPartitionKey).toBe(partitionKey);
+  });
+
+  it("Returns event 'hashKey' when event does not have a 'partitionKey'", () => {
+    const event = {}
+    const data = JSON.stringify(event);
+    const hashKey = crypto.createHash("sha3-512").update(data).digest("hex");
+
+    const eventPartitionKey = deterministicPartitionKey(event);
+    expect(eventPartitionKey).toBe(hashKey);
+  });
+
+  it("Returns hash of event 'partitionKey' if the key length is too large", () => {
+    const event = {
+      partitionKey: 'maxKeyGreaterThan256.maxKeyGreaterThan256.maxKeyGreaterThan256.maxKeyGreaterThan256.maxKeyGreaterThan256.maxKeyGreaterThan256.maxKeyGreaterThan256.maxKeyGreaterThan256.maxKeyGreaterThan256.maxKeyGreaterThan256.maxKeyGreaterThan256.maxKeyGreaterThan256.maxKeyGreaterThan256.maxKeyGreaterThan256'
+    }
+    const maxPartitionHash = crypto.createHash("sha3-512").update(event.partitionKey).digest("hex");
+    const eventPartitionKey = deterministicPartitionKey(event);
+    expect(eventPartitionKey).toBe(maxPartitionHash);
   });
 });
